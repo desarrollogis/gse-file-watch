2ac2dc5af3e8388cc7a527be7a5b00ea14771405
Vicente Obregon <desarrollogis@gmail.com>
2023-02-03 20:26:40 -0700

    Install.

--- gse-file-watch@desarrollogis.com/extension.mk
+++ gse-file-watch@desarrollogis.com/extension.mk
@@ -2,10 +2,14 @@
 EXTENSION_NAME=$(shell basename $(PWD))
 
 all-extension:
+	@echo make extension-install
 	@echo make extension-enable
 	@echo make extension-disable
 	@echo make extension-full-debug
 	@echo make extension-debug
+
+extension-install: ~/.local/share/gnome-shell/extensions/$(EXTENSION_NAME)
+	@echo run alt-f2 and then r to restart gnome
 
 extension-enable:
 	gnome-extensions enable $(EXTENSION_NAME)
@@ -18,3 +22,6 @@ extension-full-debug:
 
 extension-debug:
 	journalctl -f -o cat /usr/bin/gnome-shell GNOME_SHELL_EXTENSION_UUID=$(EXTENSION_NAME)
+
+~/.local/share/gnome-shell/extensions/$(EXTENSION_NAME):
+	ln -s $(PWD) $@
